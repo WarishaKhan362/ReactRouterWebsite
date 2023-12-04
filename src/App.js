a4dd5380e2a34ebac35f5d8d96865717fd8713be
Warisha Khan <142943552+WarishaKhan362@users.noreply.github.com>
2023-12-04 17:02:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Services from './component/Services';
-import Blogs from './component/Blogs';
+
 import Contacts from './component/Contacts'
 import Navbar from './component/Navbar';
 import Bike  from './component/Bike';
@@ -22,12 +21,11 @@ function App() {
     {/* <Route path='/' element={<Home/>} /> */}
     {/* <Route path='/about' element={<About/>} /> */}
     <Route path='/navbar' element={<Navbar/>} />
-    <Route path='/services' element={<Services/>} />
-    <Route path='/blogs' element={<Blogs/>} />
     <Route path='/contacts' element={<Contacts/>} />
     <Route path='/bike' element={<Bike/>} />
     <Route path='/car' element={<Car/>} />
     <Route  path='/festivals' element={<Festivals></Festivals>}/>
+   <Route path='/contacts' element={<Contacts/>} />
 
   
 
